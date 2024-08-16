951cdd64e3a8fa8f78be9baf79f44f31fbddad3e
Josh Davis <josh.davis@enable.com>
2024-08-16 12:42:06 -0400

    Add... 'camel'

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { BehaviorSubject, Observable, Subject } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+
   title = 'camel-clicker';
+
+  public points$ = new BehaviorSubject<number>(0);
+
+  public onClick() {
+    this.points$.next(this.points$.value + 1);
+  }
 }
